d20dd72f0de2c4d859a9fa8082ae8683c10e5a06
jian <jan8611@163.com>
2016-04-08 14:07:20 +0800

    add pages

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 var express=require('express');
+var path=require("path");//静态资源处理
 var port=process.env.PORT || 3000;
 var app=express();
 
-app.set('views','./views');
+app.set('views','./views');//视图所在目录
 app.set('view engine','jade');
+app.use(express.static(path.join(__dirname,'bower_components')));//静态资源处理
+app.use(require('body-parser')());
 app.listen(port);
 
 console.log("imooc started on "+port);
 
 app.get('/',function(req,res){
-	res.render('index.jade',{
-		title:"电影网站首页"
+	res.render('pages/index.jade',{
+		title:"电影网站首页",
+		movies:[]
 	});
 });
 
 app.get('/movie/:id',function(req,res){
-	res.render('detail.jade',{
-		title:'电影详情页面'
+	res.render('pages/detail.jade',{
+		title:'电影详情页面',
+		movie:{}
 	});
 });
 
 app.get('/admin/movie',function(req,res){
-	res.render('admin.jade',{
-		title:'后台管理--新增电影页面'
+	res.render('pages/admin.jade',{
+		title:'后台管理--新增电影页面',
+		movie:{}
 	});
 });
 
 app.get('/admin/list',function(req,res){
-	res.render('list.jade',{
-		title:'后台管理--电影列表页面'
+	res.render('pages/list.jade',{
+		title:'后台管理--电影列表页面',
+		movies:[]
 	});
 });
