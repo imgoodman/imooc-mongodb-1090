34576beb7781ed3606367f756dcdc387029011d4
jian <jan8611@163.com>
2016-04-08 19:12:26 +0800

    modules

---
+++ gruntfile.js
@@ -0,0 +1,7 @@
+module.exports=function(grunt){
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-concurrent');
+
+	grunt.option('force',true);
+}
