aef6fcd968709583bb3f1878d082207981934fb7
jian <jan8611@163.com>
2016-04-08 12:54:45 +0800

    add lots of pages

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ app.set('view engine','jade');
 app.listen(port);
 
 console.log("imooc started on "+port);
+
+app.get('/',function(req,res){
+	res.render('index.jade',{
+		title:"电影网站首页"
+	});
+});
+
+app.get('/movie/:id',function(req,res){
+	res.render('detail.jade',{
+		title:'电影详情页面'
+	});
+});
+
+app.get('/admin/movie',function(req,res){
+	res.render('admin.jade',{
+		title:'后台管理--新增电影页面'
+	});
+});
+
+app.get('/admin/list',function(req,res){
+	res.render('list.jade',{
+		title:'后台管理--电影列表页面'
+	});
+});
